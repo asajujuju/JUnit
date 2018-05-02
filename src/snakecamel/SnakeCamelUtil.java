18a932bc38f26e991ef89ccc7e07b18d8d058be5
asajujuju <g16955yf@gm.tsuda.ac.jp>
2018-05-02 15:57:23 +0900

    課題3−2終了

--- src/snakecamel/SnakeCamelUtil.java
+++ src/snakecamel/SnakeCamelUtil.java
@@ -3,44 +3,46 @@ package snakecamel;
 public class SnakeCamelUtil {
 
 	public static String snakeToCamelcase(String snake_case) {
-		String[] words = snake_case.split("_");
+		String[] words = snake_case.split("_");//words[0]=abc, words[1]=def, words[2]=ghi
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < words.length - 1; i++) {
+		for (int i = 0; i < words.length-1; i++) {
 			sb.append(capitalize(words[i]));
 		}
-		return new String(sb);
+		return new String(sb); //ひとつの単語にまとめる(AbcDefGhi)
 	}
 	 
-	public static String camelToSnakecase(String camelcase) {
-		StringBuilder sb = new StringBuilder();
-		int j = 0;
-		for (int i = 0; i < camelcase.length(); i++) {
-			char c = camelcase.charAt(i);
-			if (Character.isUpperCase(c)) {
-				sb.append(camelcase.substring(j, i));
-				if (sb.length() > 0) {
-					sb.append("_");
+		public static String camelToSnakecase(String camelcase) {
+			StringBuilder sb = new StringBuilder();
+			int j = 0;
+			for (int i = 0; i < camelcase.length(); i++) {
+				char c = camelcase.charAt(i);//camelのi番目を抜き出す
+				if (Character.isUpperCase(c)) { //大文字なら
+						sb.append(camelcase.substring(j, i));//camelcaseのj~iまでの文字を加える(iは含まれない)
+					if (sb.length() > 0) {
+						sb.append("_");
+					}
+					sb.append(Character.toLowerCase(c));//小文字に変換
+					j=i;
 				}
-				sb.append(Character.toLowerCase(c));
-				j = i;
 			}
+			sb.append(camelcase.substring(j));//j番目から末尾まで
+			return new String(sb);
 		}
-		sb.append(camelcase.substring(j));
-		return new String(sb);
-	}
+		
+		
+		static String capitalize(String s) {//1文字目を大文字にするクラス
+			char first = s.charAt(0);
+			char upperFirst = Character.toUpperCase(first);//大文字に変換
+			String rest = s.substring(1);
+			return upperFirst + rest;
+		}
 	
-	static String capitalize(String s) {
-		char first = s.charAt(0);
-		char upperFirst = Character.toUpperCase(first);
-		String rest = s.substring(1);
-		return upperFirst + rest;
+		static String uncapitalize(String s) {
+			char first = s.charAt(0);
+			char lowerFirst = Character.toLowerCase(first);
+			String rest = s.substring(1);
+			return lowerFirst + rest;
+		}
+		
+		
 	}
-
-	static String uncapitalize(String s) {
-		char first = s.charAt(0);
-		char lowerFirst = Character.toLowerCase(first);
-		String rest = s.substring(1);
-		return lowerFirst + rest;
-	}
-	
-}
---
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -0,0 +1,15 @@
+package snakecamel;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SnakeCamelUtilTest {
+
+	@Test
+	public void test() {
+		System.out.print("not yet implemented");
+	}
+
+}
