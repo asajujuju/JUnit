03ccb7989a86df86c9a5af446da241a8205021ff
asajujuju <g16955yf@gm.tsuda.ac.jp>
2018-05-02 16:17:25 +0900

    snaketocamel成功

--- test/snakecamel/SnakeCamelUtilTest.java
+++ test/snakecamel/SnakeCamelUtilTest.java
@@ -7,13 +7,13 @@ import org.junit.Test;
 
 public class SnakeCamelUtilTest {
 
-     @org.junit.Test
-     public void snaketocamelの確認() {
-    	 SnakeCamelUtil scu = new SnakeCamelUtil();
-    	 String expected = "HotDog";
-    	 String actual = scu.snakeToCamelcase("hot_dog");
-    	 assertThat(actual, is(expected));
-     }
+	@org.junit.Test
+    public void snaketocamelの確認() {
+   	 SnakeCamelUtil scu = new SnakeCamelUtil();
+   	 String expected = "HotDog";
+   	 String actual = scu.snakeToCamelcase("hot_dog");
+   	 assertThat(actual, is(expected));
+    }
      
 
 }
